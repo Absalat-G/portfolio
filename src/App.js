db9a4186ade02897f444a2600cd72f1bc892491e
Absalat-G <workalemahuabsalat2011@gmail.com>
2023-02-02 21:55:25 -0800

    added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-
+import Button from '@mui/material/Button';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div>
+<h1>material ui and tailwind</h1>
+<Button variant="contained" className="bg-black">Contained</Button>
+   </div>
   );
 }
 
