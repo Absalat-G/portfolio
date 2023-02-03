7f87fb5d3276328d9799e357f0a27cf7f6971c43
Absalat-G <workalemahuabsalat2011@gmail.com>
2023-02-02 21:59:01 -0800

    added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import './App.css';
-import Button from '@mui/material/Button';
+
 function App() {
   return (
    <div>
-<h1>material ui and tailwind</h1>
-<Button variant="contained" className="bg-black">Contained</Button>
+<Home/>
    </div>
   );
 }
---
+++ src/Components/Home.js
@@ -0,0 +1,14 @@
+
+import React from 'react'
+import AppBar from '@mui/material'
+const Home = () => {
+  return (
+    <div> 
+<AppBar>
+    
+</AppBar>
+    </div>
+  )
+}
+
+export default Home
